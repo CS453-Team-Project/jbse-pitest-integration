f4d1b576df53ed930054ca30777286b47424b769
Keonwoo Kim <keonwoo@kaist.ac.kr>
2021-05-05 06:11:10 +0000

    complex example

--- src/main/java/com/cs453/group5/examples/Calculator.java
+++ src/main/java/com/cs453/group5/examples/Calculator.java
@@ -1,16 +1,24 @@
 package com.cs453.group5.examples;
 
+import java.util.*;
 import static jbse.meta.Analysis.ass3rt;
 
 public class Calculator {
-    public int getSign(int number) {
-        int result = 0;
-        if (number > 0) {
-            result = 1;
-        } else if (number < 0) {
-            result = -1;
-        }
-        // ass3rt(result == 1);
-        return result;
+    public void getSign() {
+        Scanner input = new Scanner(System.in);
+		int t = input.nextInt();
+		for(int test = 0; test < t; test++){
+			int n = input.nextInt();
+			int l = input.nextInt();
+			int r = input.nextInt();
+			int[] c = new int[n + 1];
+			int count = 0;
+			for(int i = 0; i < l; i++){
+				c[input.nextInt()]++;
+			}
+			for(int i = 0; i < r; i++){
+				c[input.nextInt()]--;
+			}
+		}
     }
 }
--- src/test/java/com/cs453/group5/examples/TestCalculator.java
+++ src/test/java/com/cs453/group5/examples/TestCalculator.java
@@ -6,17 +6,17 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestCalculator {
-    @DisplayName("Get the sign of a positive integer.")
-    @Test
-    public void test1() {
-        Calculator obj = new Calculator();
-        assertEquals(1, obj.getSign(10));
-    }
+    // @DisplayName("Get the sign of a positive integer.")
+    // @Test
+    // public void test1() {
+    //     Calculator obj = new Calculator();
+    //     assertEquals(4, obj.getSign(4, 5, 7));
+    // }
 
-    @DisplayName("Get the sign of the zero.")
-    @Test
-    public void test2() {
-        Calculator obj = new Calculator();
-        assertEquals(0, obj.getSign(0));
-    }
+    // @DisplayName("Get the sign of the zero.")
+    // @Test
+    // public void test2() {
+    //     Calculator obj = new Calculator();
+    //     assertEquals(0, obj.getSign(0));
+    // }
 }
